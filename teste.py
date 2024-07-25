70707dacf0785b1e9bf13a048fa3a9c231bc1e4c
AmiltonOsousa <119018582+AmiltonOsousa@users.noreply.github.com>
2024-07-25 10:26:11 -0300

    att - correção de erros automático

--- teste.py
+++ teste.py
@@ -1,12 +1,20 @@
 import os
 import re
 
-
 def extract_number(filename):
-    # Extrai o número do nome do arquivo original
-    match = re.search(r'\d+', filename)
-    return int(match.group()) if match else float('inf')  # Retorna infinito se nenhum número for encontrado
+    match = re.findall(r'\d+', filename)
+    if match:
+        full_number = ''.join(match)
+        return int(full_number)
+    else:
+        return float('inf')
 
+def clean_filename(filename):
+    # Remove espaços extras e caracteres especiais
+    filename = filename.strip()
+    filename = re.sub(r'[^\w\s-]', '', filename)  # Remove caracteres que não são alfanuméricos, underscore (_) ou hífen (-)
+    filename = re.sub(r'\s+', ' ', filename)  # Substitui múltiplos espaços por um único espaço
+    return filename
 
 def rename_pdfs(directory, names_file):
     if not os.path.exists(directory):
@@ -30,20 +38,20 @@ def rename_pdfs(directory, names_file):
         print("A quantidade de novos nomes não corresponde à quantidade de arquivos PDF.")
         return
 
-    # Ordena os arquivos PDF com base nos números extraídos dos nomes originais
     pdf_files_sorted = sorted(pdf_files, key=extract_number)
 
     for old_name, new_name in zip(pdf_files_sorted, new_names):
         old_path = os.path.join(directory, old_name)
-        new_path = os.path.join(directory, new_name + '.pdf')
+        new_name_clean = clean_filename(new_name)
+        new_path = os.path.join(directory, new_name_clean + '.pdf')
 
         try:
             os.rename(old_path, new_path)
-            print(f'Renomeado: {old_name} -> {new_name}.pdf')
+            print(f'Renomeado: {old_name} -> {new_name_clean}.pdf')
         except Exception as e:
-            print(f"Erro ao renomear {old_name} para {new_name}.pdf: {e}")
+            print(f"Erro ao renomear {old_name} para {new_name_clean}.pdf: {e}")
 
-
-directory = r'C:\Users\x545208\Desktop\Renamer\pdfs'
-names_file = r'C:\Users\x545208\Desktop\Renamer\lista-de-nomes.txt'
+# Exemplo de utilização:
+directory = r'C:\Users\d829574\Desktop\Renamer-master\pdfs'
+names_file = r'C:\Users\d829574\Desktop\Renamer-master\lista-de-nomes.txt'
 rename_pdfs(directory, names_file)
